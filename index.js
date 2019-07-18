f647e8bc2dda269eecef778b67b01b944e6fd618
jonathan Aubut <jo.aubut@gmail.com>
2019-07-18 00:15:00 -0400

    ConnexionBD

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Ceci est l'entré du programme
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host : '69.17.245.17',
+    port : '3306',
+    user : 'link852258',
+    password : 'ganon753',
+    database : 'WEBCADEAU'
+});
+
+conn.connect((err)=>{
+    if(err){
+        console.error(err.stack);
+        return;
+    }
+    console.log('connected as id ' + conn.threadId);
+    conn.end();
+});
