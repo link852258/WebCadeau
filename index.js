f827fa67636d065d66aab93b411fab8b79b92ef5
jonathan Aubut <jo.aubut@gmail.com>
2019-07-19 01:41:41 -0400

    Inscription Fonctionel

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 //**********SERVER**********//
 const express = require('express');
-var BD = require('./BD.js');
+const bodyParser = require('body-parser')
+const BD = require('./BD.js');
 const app = express();
 const PORT = 3000;
 var conn = new BD();
-conn.ouvrirconnexion();
 //ouvre la connection à la bd
+conn.ouvrirconnexion();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', __dirname + '/vues');
 
@@ -29,7 +31,14 @@ app.post('/Connexion',(req,res)=>{
 app.get('/Inscription',(req,res)=>{
     res.render('Inscription');
 });
+
 app.post('/Inscription',(req,res)=>{
+    const username = req.body.fNomUtilisateur;
+    const password = req.body.fMotDePasse;
+    const email = req.body.fEmail;
+    const prenom = req.body.fPrenom;
+    const nom = req.body.fNom;
+    conn.insererUtilisateur(username,password,email,prenom,nom);
     res.render('Inscription');
 });
 
