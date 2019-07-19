cb4aec01413252a19a9456b5208717f4d9d4bdd7
jonathan Aubut <jo.aubut@gmail.com>
2019-07-18 23:21:29 -0400

    Ajout index et autres

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //Ceci est l'entré du programme
-const mysql = require('mysql');
+/*const mysql = require('mysql');
 const conn = mysql.createConnection({
     host : '69.17.245.17',
     port : '3306',
-    user : 'link852258',
-    password : 'ganon753',
+    user : 'WEBCADEAU',
+    password : 'Webcadeau!',
     database : 'WEBCADEAU'
 });
 
@@ -15,4 +15,19 @@ conn.connect((err)=>{
     }
     console.log('connected as id ' + conn.threadId);
     conn.end();
+});*/
+
+//**********SERVER**********//
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('views', __dirname + '/vues');
+
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    res.render('Index');
 });
+
+app.listen(PORT);
