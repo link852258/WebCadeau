7af3eaf3be58b061af3469fcba359c289b379c58
jonathan Aubut <jo.aubut@gmail.com>
2019-08-28 22:15:45 -0400

    Boostrap

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('css'));
 app.use(express.static('js'));
+app.use(express.static('img'));
 
 app.set('views', __dirname + '/vues');
 
